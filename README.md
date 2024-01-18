ac9cdb44e5a9c01c5424a8cb185a9d09d6e2441a
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-18 23:46:02 +0300

    second commit

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Actions Status](https://github.com/DmitryK1995/typescript-developer-project-81/actions/workflows/typescript.yml/badge.svg)](https://github.com/DmitryK1995/typescript-developer-project-81/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/91ae32ae9b3188116a16/maintainability)](https://codeclimate.com/github/DmitryK1995/typescript-developer-project-81/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/91ae32ae9b3188116a16/test_coverage)](https://codeclimate.com/github/DmitryK1995/typescript-developer-project-81/test_coverage)
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
+/* eslint-disable no-underscore-dangle */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { expect, test } from 'vitest';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path, { dirname } from 'path';
+
 import Tag from './index.ts';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('test 1', () => {
   expect(new Tag('label', { for: 'email' }, 'Email').toString()).toBe('<label for="email">Email</label>');
+});
+
+test('test 2', () => {
+  expect(readFile('file1.json')).toBe(readFile('file2.json'));
 });
