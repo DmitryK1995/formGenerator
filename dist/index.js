b2b5afd1d0fb90a88ee012973f8e2bec1672add8
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-19 17:31:56 +0300

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const FormFieldsCreator_1 = tslib_1.__importDefault(require("./modules/FormFieldsCreator"));
-const tag_1 = tslib_1.__importDefault(require("./modules/tag"));
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
+Object.defineProperty(exports, '__esModule', { value: true });
+const tslib_1 = require('tslib');
+const FormFieldsCreator_1 = tslib_1.__importDefault(require('./modules/FormFieldsCreator'));
+const tag_1 = tslib_1.__importDefault(require('./modules/tag'));
+
 class HexletCode {
-    static formFor(template, formInfo, func) {
-        const formProperties = {
-            method: `${formInfo.method ? formInfo.method : 'post'}`,
-            action: `${formInfo.url ? formInfo.url : '#'}`,
-        };
-        const inputFieldsNew = new FormFieldsCreator_1.default(template);
-        func(inputFieldsNew);
-        const inputFields = inputFieldsNew.getInputList;
-        return new tag_1.default('form', formProperties, inputFields).toString();
-    }
+  static formFor(template, formInfo, func) {
+    const formProperties = {
+      method: `${formInfo.method ? formInfo.method : 'post'}`,
+      action: `${formInfo.url ? formInfo.url : '#'}`,
+    };
+    const inputFieldsNew = new FormFieldsCreator_1.default(template);
+    func(inputFieldsNew);
+    const inputFields = inputFieldsNew.getInputList;
+    return new tag_1.default('form', formProperties, inputFields).toString();
+  }
 }
 exports.default = HexletCode;
-//# sourceMappingURL=index.js.map
+// # sourceMappingURL=index.js.map
--- dist/modules/helpers/form-helpers.js
+++ dist/modules/helpers/form-helpers.js
@@ -1,13 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const tag_1 = tslib_1.__importDefault(require("../tag"));
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
+/* eslint-disable import/no-extraneous-dependencies */
+Object.defineProperty(exports, '__esModule', { value: true });
+const tslib_1 = require('tslib');
+const tag_1 = tslib_1.__importDefault(require('../tag'));
+
 function createLabel(forValue) {
-    const value = forValue.charAt(0).toUpperCase() + forValue.slice(1);
-    const labelProperties = {
-        for: forValue,
-    };
-    return new tag_1.default('label', labelProperties, value).toString();
+  const value = forValue.charAt(0).toUpperCase() + forValue.slice(1);
+  const labelProperties = {
+    for: forValue,
+  };
+  return new tag_1.default('label', labelProperties, value).toString();
 }
 exports.default = createLabel;
-//# sourceMappingURL=form-helpers.js.map
+// # sourceMappingURL=form-helpers.js.map
